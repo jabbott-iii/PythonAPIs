3b47f18fc7d797e07eb7e4aa90ac04f21043e7b7
Joseph Abbott III <code.jabbott.iii@gmail.com>
2026-01-17 16:24:54 -0700

    created image module

---
+++ test/app/images.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+from imagekitio import ImageKit
+import os
+
+load_dotenv()                                                                              # Load environment variables from .env file
+imagekit = ImageKit(
+    private_key=os.getenv("IMAGEKIT_PRIVATE_KEY"),
+    public_key=os.getenv("IMAGEKIT_PUBLIC_KEY"),
+    url_endpoint=os.getenv("IMAGEKIT_URL_ENDPOINT")
+)
