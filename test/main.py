79cacd17cc608add98eef0d84f3d7b0d6107feb6
Joseph Abbott III <code.jabbott.iii@gmail.com>
2026-01-16 22:08:29 -0700

    defined uvicorn webserver

--- test/main.py
+++ test/main.py
@@ -1,6 +1,4 @@
-def main():
-    print("Hello from test!")
-
+import uvicorn # webserver for FastAPI
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True) # app.app:app referes to app folder, app.py file, and app variable
