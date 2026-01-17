3b7bca7b2b9493232654a6d6b38c168a0276519e
Joseph Abbott III <code.jabbott.iii@gmail.com>
2026-01-17 14:41:49 -0700

    test.db created from successful setup of db.py

--- test/app/db.py
+++ test/app/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
 
-DATABASE_URL = "sqlite_+aiosqlite:///./test.db"                                                              # database connection string
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"                                                               # database connection string
 
 utc_now = datetime.datetime.now(datetime.timezone.utc)                                                       # utc variable necessary for timestamping
 
