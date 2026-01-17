8f9cf8e588ec1e0461f10bb00609377dc87ab128
Joseph Abbott III <code.jabbott.iii@gmail.com>
2026-01-17 13:41:48 -0700

    -> PostResponse schema restriction/validation

--- test/app/app.py
+++ test/app/app.py
@@ -10,13 +10,13 @@ def get_posts():
     return textposts
 
 @app.get("/posts/{post_id}")
-def get_post(post_id: int):
+def get_post(post_id: int) -> schemas.PostResponse:
     if post_id not in textposts:
         raise fastapi.HTTPException(status_code=404, detail="Post not found")
     return textposts.get(post_id)
 
 @app.post("/posts")
-def create_post(post: schemas.PostCreate):
+def create_post(post: schemas.PostCreate) -> schemas.PostResponse: # validation to PostResponse to protect against injection / inappropriate fields ( -> schema)
     new_post = {"title": post.title, "content": post.content, "published": post.published}
     textposts[max(textposts.keys(), default=0) + 1] = new_post
     return new_post
--- test/app/schemas.py
+++ test/app/schemas.py
@@ -4,3 +4,8 @@ class PostCreate(pydantic.BaseModel):
     title: str
     content: str
     published: bool = True
+
+class PostResponse(pydantic.BaseModel):
+    title: str
+    content: str
+    published: bool = True
