2d6e43f21ef0c249002b179ef9cc28a47a54640b
Joseph Abbott III <code.jabbott.iii@gmail.com>
2026-01-19 22:10:03 -0700

    modified server instruction for app folder

--- ministral3_3b_API/main.py
+++ ministral3_3b_API/main.py
@@ -1,4 +1,4 @@
 import uvicorn # webserver for FastAPI
 
 if __name__ == "__main__":
-    uvicorn.run("ministral3_3b_API.app:app", host="0.0.0.0", port=8000, reload=True) # ministral3_3b_API.app:app referes to ministral3_3b_API folder, app.py file, and app variable
+    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True) # app.app:app refers to app folder, app.py file, and app variable
