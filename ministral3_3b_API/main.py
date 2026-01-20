9eff40b16d2c1dd8544a9f1ef908b18b2dd80aa2
Joseph Abbott III <code.jabbott.iii@gmail.com>
2026-01-19 22:06:03 -0700

    added informative comments

--- ministral3_3b_API/main.py
+++ ministral3_3b_API/main.py
@@ -1,4 +1,4 @@
-import uvicorn
+import uvicorn # webserver for FastAPI
 
 if __name__ == "__main__":
-    uvicorn.run("ministral3_3b_API.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("ministral3_3b_API.app:app", host="0.0.0.0", port=8000, reload=True) # ministral3_3b_API.app:app referes to ministral3_3b_API folder, app.py file, and app variable
