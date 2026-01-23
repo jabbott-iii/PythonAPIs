07797d6af28d58e82f8b1006e432677395cb3cdd
Joseph Abbott III <code.jabbott.iii@gmail.com>
2026-01-22 23:54:57 -0700

    current_weather

--- weather_hourly/app/app.py
+++ weather_hourly/app/app.py
@@ -9,29 +9,15 @@ def get_dataframe():
 
 @app.get('/cities')
 def get_cities():
-    
-    cities = {
-        "New York": {"latitude": 40.71, "longitude": -74.01},
-        "Los Angeles": {"latitude": 34.05, "longitude": -118.24},
-        "Chicago": {"latitude": 41.88, "longitude": -87.63},
-        "Houston": {"latitude": 29.76, "longitude": -95.37},
-        "Phoenix": {"latitude": 33.45, "longitude": -112.07},
-        "Philadelphia": {"latitude": 39.95, "longitude": -75.17},
-        "San Antonio": {"latitude": 29.43, "longitude": -98.49},
-        "San Diego": {"latitude": 32.72, "longitude": -117.16},
-        "Dallas": {"latitude": 32.47, "longitude": -96.48},
-        "San Jose": {"latitude": 37.34, "longitude": -121.89},
-    }
-    
     for response in data.responses:
         yield {
             "latitude": response.Latitude(),
             "longitude": response.Longitude(),
             "elevation": response.Elevation(),
-            'hourly_temperature_2m': data.hourly_temperature_2m,
-            'hourly_relative_humidity_2m': data.hourly_relative_humidity_2m,
-            'hourly_precipitation': data.hourly_precipitation,
-            'hourly_precipitation_probability': data.hourly_precipitation_probability
+            'hourly_temperature_2m': data.response.current_weather().Temperature2m(),
+            'hourly_relative_humidity_2m': data.response.current_weather().RelativeHumidity2m(),
+            'hourly_precipitation': data.response.current_weather().Precipitation(),
+            'hourly_precipitation_probability': data.response.current_weather().PrecipitationProbability()
         }
 
-        # for this get, instead buuild a while new SQL database for the hourly inputs and have it auto delete old entries everytime a new one is entered?
+      # figure out how to apply only current weather attribute to each city
