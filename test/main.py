91792b0d8671b74406c5c9be26948a49539368f4
Joseph Abbott III <code.jabbott.iii@gmail.com>
2026-02-27 19:18:34 -0700

    Delete test directory

--- test/main.py
+++
@@ -1,4 +0,0 @@
-import uvicorn # webserver for FastAPI
-
-if __name__ == "__main__":
-    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True) # app.app:app referes to app folder, app.py file, and app variable
